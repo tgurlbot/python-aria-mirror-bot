28c89d1aa3df8448179576579ad60dd3b33082d4
tgurlbot <64328438+tgurlbot@users.noreply.github.com>
2020-05-31 17:46:18 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -5,3 +5,4 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+   --check-certificate=false
